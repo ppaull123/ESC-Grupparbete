0e35acf7f3acb324756d0fff78feb554f610e5a1
filip <filip.sarik@lerniastudent.se>
2025-11-13 17:01:14 +0100

    (step 1)
    "Book this room" buttons öppnar nu en "modal". Stylat första rutan (step 1)

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ closeBtn.addEventListener("click", function () {
     document.querySelector("body").classList.toggle('menu-open');
     document.querySelector("header").classList.toggle('menu-open');
 });
+
+// modal ruta
+const modal = document.querySelector('.modal-overlay');
+const openButtons = document.querySelectorAll('.btn--book');
+const closeButton = document.querySelector('.modal-close');
+
+openButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+  });
+});
+
+closeButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
