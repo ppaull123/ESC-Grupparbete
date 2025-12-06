d77c55dc17d9b9606c7ca7a65d30bd472e81f281
ValentynaVoronkova <vella.dona@gmail.com>
2025-12-06 11:09:29 +0100

    filter form as modal, I don't like logic

--- challenges.js
+++ challenges.js
@@ -4,9 +4,11 @@ import { wrapper, loadAllChallenges } from "./challengecard.js"
 //open filter form
 const challengesFilterButton = document.querySelector('.btn-challengesFilter');
 const filterForm = document.querySelector('.filterForm');
+const wrapFilterArea = document.querySelector('.wrap-filterArea');
 
 challengesFilterButton.addEventListener('click', () => {
     filterForm.classList.toggle('filterForm--active');
+    wrapFilterArea.classList.toggle('wrap-filterArea--active');
 })
 
 const noMatchesInfo = document.querySelector('.filterForm__info');
@@ -17,6 +19,7 @@ filterForm__closeBtn.style.cursor = 'pointer';
 
 filterForm__closeBtn.addEventListener('click', async () => {
     filterForm.classList.remove('filterForm--active');
+    wrapFilterArea.classList.remove('wrap-filterArea--active');
 
     //render all cards after filter form is closed
     //clear up filters from previous input
