089add8f25d5498680a8701a6ab0ee15792ad808
ValentynaVoronkova <vella.dona@gmail.com>
2025-12-08 16:50:56 +0100

    fix mobile menu, add hover effect and brighten links of open pages in the menu

--- index.js
+++ index.js
@@ -20,25 +20,51 @@ closeBtn.addEventListener("click", function () {
 
 //Open "see all challenges" page
 const seeChallenges = document.querySelector(".section-play-online .btn-teambuilding");
-seeChallenges.addEventListener("click", () => {
-    window.location.href = "challenges.html";
-});
+if (seeChallenges) {
+    seeChallenges.addEventListener("click", () => {
+        window.location.href = "challenges.html";
+    });
+}
+
 //  ONLINE BUTTONS 
 const onlineBtns = document.querySelectorAll(".btn--online");
-
-onlineBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        // Go to challenges page and set hash to #online
-        window.location.href = "challenges.html#online";
+if (onlineBtns.length) {
+    onlineBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            // Go to challenges page and set hash to #online
+            window.location.href = "challenges.html#online";
+        });
     });
-});
+}
 
 //  ON-SITE BUTTONS 
 const onsiteBtns = document.querySelectorAll(".btn--onSite");
-
-onsiteBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        // Go to challenges page and set hash to #onsite
-        window.location.href = "challenges.html#onsite";
+if (onsiteBtns.length) {
+    onsiteBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            // Go to challenges page and set hash to #onsite
+            window.location.href = "challenges.html#onsite";
+        });
     });
+}
+
+//close mobilemenu when any link is clicked
+const navLinks = document.querySelectorAll(".nav-menuContent a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+        document.body.classList.remove("menu-open");
+    });
+});
+
+// //show/ brighten the link to the active page in the menu
+const currentURL = window.location.pathname.split('/').pop();
+
+document.querySelectorAll('nav a, footer a').forEach(link => {
+    const linkURL = link.getAttribute('href');
+
+    if (linkURL === currentURL) {
+        link.classList.add('active');
+    }
 });
